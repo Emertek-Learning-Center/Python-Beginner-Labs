7325a0664c13a4600c531a28a8d52cb7c1e85a11
DevOps-T04 <91058125+DevOps-T04@users.noreply.github.com>
2022-02-24 12:21:10 +0100

    lab02

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-#list of all types
-list=[5,20,12,58,58,5,2,63,9,3,1,78,47,15,"hello5",12.5,"yo",20]
+#list of dictionary
+student=[
+  {
+    "name":"alex",
+    "age":20,
+    "code":"AB02",
+    "notes":(12,5,8,20,14.5)
+  },
+  {
+    "name":"mourad",
+    "age":42,
+    "code":"AB01",
+    "notes":(10.25,5.5,8.3,18,10.5)
+  },
+  {
+    "name":"gaby",
+    "age":260,
+    "code":"AB03",
+     "notes":(11,0,16.25,19,14,5)
+  },
+  {
+    "name":"omar",
+    "age":31,
+    "code":"AB04",
+    "notes":(17,15,18,20,14,15)
+  }
+]
 
-def get5(list):
-  #your source code here
+#should print the averge of each student
+def studentAverge(student):
+  # your code here
 
-
-#show new list
-print(get5(list))
+#call function
+studentAverge(student)
